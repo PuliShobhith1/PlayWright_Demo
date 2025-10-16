3c3ebb35086cd279c4233a800c5ec292e90b5fc7
Your Name <your.email@example.com>
2025-10-16 12:04:11 +0530

    update done

--- tests/UI_Test/Orangehrmlive_CreatUniqueUser.Spec.ts
+++ tests/UI_Test/Orangehrmlive_CreatUniqueUser.Spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Create Unique For Orangehrmlive @smoke', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await expect(page.getByRole('img', { name: 'company-branding' })).toBeVisible();
   await page.getByRole('textbox', { name: 'Username' }).click();
